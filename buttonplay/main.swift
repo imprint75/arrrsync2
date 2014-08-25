e9dc463217a9d385adeb681dc72b98194d33a35f
sean smith <seansmith@seans-MacBook-Pro-2.local>
2014-08-25 23:25:41 +0300

    Initial Commit

---
+++ buttonplay/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  buttonplay
+//
+//  Created by sean smith on 8/25/14.
+//  Copyright (c) 2014 ss. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
